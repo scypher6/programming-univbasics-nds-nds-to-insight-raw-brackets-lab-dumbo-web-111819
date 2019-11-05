350ee6e3ec47bf78f80c5c35b825d0a3b93ca8e6
Joseph Pyram <joseph.pyram@gmail.com>
2019-11-05 22:26:08 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,21 +2,16 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  nil
+  db = nds
+  result = {}                 #hash containing directors/gross sales pairs
+
+  db.size.times do |odex|     #odex: outter index
+      gross_sales = 0         #Set, reset gross sales for each element of the db array
+      db[odex][:movies].size.times do |index|         #loop based on the size of the inner arrays for each director
+          gross_sales += db[odex][:movies][index][:worldwide_gross]     #compute gross sales
+      end #INNER loop
+      key = db[odex][:name]       #assign director names
+      result[key] = gross_sales   #build the hash
+  end #OUTTER lop
+  result
 end
