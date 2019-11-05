4d1a833afe9e31b41ad0e86e690a9153a1ac1ab4
Joseph Pyram <joseph.pyram@gmail.com>
2019-11-05 22:25:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,15 +3,15 @@ require 'directors_database'
 
 def directors_totals(nds)
   db = nds
-  result = {}
+  result = {}                 #hash containing directors/gross sales pairs
 
-  db.size.times do |odex|
-      gross_sales = 0         #Set, reset gross sales
-      db[odex][:movies].size.times do |index|
-          gross_sales += db[odex][:movies][index][:worldwide_gross]
+  db.size.times do |odex|     #odex: outter index
+      gross_sales = 0         #Set, reset gross sales for each element of the db array
+      db[odex][:movies].size.times do |index|         #loop based on the size of the inner arrays for each director
+          gross_sales += db[odex][:movies][index][:worldwide_gross]     #compute gross sales
       end #INNER loop
-      key = db[odex][:name]
-      result[key] = gross_sales
+      key = db[odex][:name]       #assign director names
+      result[key] = gross_sales   #build the hash
   end #OUTTER lop
   result
 end
